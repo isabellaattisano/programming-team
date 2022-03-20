8b830c6b1898c2178174341585499e78285edc39
isabellaattisano <iattisan@villanova.edu>
2022-03-20 14:25:37 -0400

    update

--- week18/sort_numbers.py
+++
@@ -1,6 +0,0 @@
-a, b = input().split()
-
-if(int(a)>int(b)):
-    print(b,a)
-else:
-    print(a,b)
--- week8/poker_hand.py
+++
@@ -1,7 +0,0 @@
-list = [a[0] for a in input().split()]
-max = 0
-for i in range(len(list)):
-    if list.count(list[i]) > max:
-        max = list.count(list[i])
-
-print(max)
